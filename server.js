9f3bd1f748b1400a8ca0ec5354b75582f6913601
venusofstar <sumidomikeangelo24@gmail.com>
2025-12-15 23:09:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const streams = {
     "https://manifest.googlevideo.com/api/manifest/hls_variant/expire/1765826181/ei/JQpAaYenG67H0-kP37Ct-Ag/ip/126.209.53.186/id/rc4KfaBrGIc.1/source/yt_live_broadcast/requiressl/yes/xpc/EgVo2aDSNQ%3D%3D/file/index.m3u8",
 
   gma:
-    "https://manifest.googlevideo.com/api/manifest/hls_variant/expire/1765809651/ei/k8k_abjZJYaq2roPkpPvuQ0/ip/180.190.223.161/id/n0q7qAEljA8.1/source/yt_live_broadcast/requiressl/yes/xpc/EgVo2aDSNQ%3D%3D/file/index.m3u8"
+    "https://manifest.googlevideo.com/api/manifest/hls_variant/expire/1765832771/ei/4yNAaf7DEvq12roPzv_emQs/ip/112.205.135.106/id/n0q7qAEljA8.1~45720746/source/yt_live_broadcast/requiressl/yes/xpc/EgVo2aDSNQ%3D%3D/hfr/1/playlist_duration/30/manifest_duration/30/maudio/1/bui/AYUSA3An7fek690wEZX1up1iMtVIuQdl7FmHkOPUsA43CCsUT9by7WcoLDaBYEwYacHi2caEjNvOmWVS/spc/wH4Qq-O-tZK9iQ6ha_5CUEO9iRPD3mtKndr1KfvOaeWHr2ZRXqOqgE-5pT8/vprv/1/go/1/rqh/5/pacing/0/nvgoi/1/ncsapi/1/keepalive/yes/fexp/51331020%2C51552689%2C51565115%2C51565682%2C51580968/dover/11/itag/0/playlist_type/DVR/sparams/expire%2Cei%2Cip%2Cid%2Csource%2Crequiressl%2Cxpc%2Chfr%2Cplaylist_duration%2Cmanifest_duration%2Cmaudio%2Cbui%2Cspc%2Cvprv%2Cgo%2Crqh%2Citag%2Cplaylist_type/sig/AJfQdSswRQIhAKypzo5sCyC6-a0F5hvOtNFXIWldO8OD1TekIxlXR-YnAiBR0hC-8otWC1-wx9Yi_kQCTrMI42gvc3KrLpK5rGFpXA%3D%3D/file/index.m3u8"
 };
 
 /**
